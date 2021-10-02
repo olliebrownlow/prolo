07a45231032c1b18c315f816a1890e75b2c1deb1
Oliver Brownlow <olliebrownlow@gmail.com>
2021-10-02 02:54:48 +0000

    refactors coin and fiat data processing into reusable  functions

--- pages/pocket.js
+++ pages/pocket.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../lib/UserContext";
 import CurrencySettingsContext from "../context/currencySettings";
 import Loading from "../components/loading";
@@ -7,13 +7,8 @@ import FiatList from "../components/fiat-list";
 import Modal from "../components/modal";
 import PocketBalance from "../components/pocket-balance";
 import Link from "next/link";
-import {
-  getCoins,
-  getConvertedAmount,
-  getCurrencySettings,
-  getCryptoData,
-  getFiat,
-} from "../actions";
+import { getCoinData } from "../lib/core/coinData";
+import { getConvertedBalanceData } from "../lib/core/convertedBalanceData";
 import styles from "../pageStyles/pocket.module.scss";
 
 const Pocket = (props) => {
@@ -70,100 +65,10 @@ const Pocket = (props) => {
 };
 
 export async function getServerSideProps() {
-  const coins = await getCoins();
-
-  const currencySettings = await getCurrencySettings();
-  const fiatConvert = currencySettings[0].currencyCode.toUpperCase();
-
-  const coinCodeArray = [];
-  coins.map((coin) => {
-    coinCodeArray.push(coin.code);
-  });
-  const coinCodes = coinCodeArray.join(",");
-
-  let coinDataFull;
-  if (coinCodes.length > 0) {
-    coinDataFull = await getCryptoData(coinCodes, fiatConvert);
-  } else {
-    coinDataFull = [];
-  }
-  // console.log(coinDataFull);
-
-  const amount = (code) => {
-    let result = 0;
-    coins.filter((coin) => {
-      if (coin.code === code) {
-        result = coin.amount;
-      }
-    });
-    return result;
-  };
-
-  let coinData;
-  if (coinDataFull.length > 0) {
-    coinData = coinDataFull.map((coin) => ({
-      id: coin.id,
-      name: coin.name.toLowerCase(),
-      logo_url: coin.logo_url,
-      price: coin.price,
-      amount: amount(coin.id),
-      total: amount(coin.id) * coin.price,
-      currencyInUse: currencySettings[0].currencyCode,
-    }));
-  } else {
-    coinData = [
-      {
-        currencyInUse: currencySettings[0].currencyCode,
-      },
-    ];
-  }
-
-  const fiatData = await getFiat();
-
-  let convertedBalanceDataFull = [];
-
-  await Promise.all(
-    fiatData.map(async (item) => {
-      convertedBalanceDataFull.push(
-        await getConvertedAmount(item.code, fiatConvert, item.amount)
-      );
-    })
-  );
-
-  const fullFiatName = (fiatCode) => {
-    let result = "unknown currency";
-    fiatData.filter((fiat) => {
-      if (fiat.code === fiatCode) {
-        result = fiat.name;
-      }
-    });
-    return result;
-  };
-
-  const fiatSign = (fiatCode) => {
-    let result = "";
-    fiatData.filter((fiat) => {
-      if (fiat.code === fiatCode) {
-        result = fiat.sign;
-      }
-    });
-    return result;
-  };
-
-  const convertedBalanceData = convertedBalanceDataFull.map(
-    (convertedBalance) => ({
-      id: convertedBalance.response.from,
-      from: convertedBalance.response.from,
-      to: convertedBalance.response.to,
-      amount: convertedBalance.response.amount,
-      value: convertedBalance.response.value,
-      fullFiatName: fullFiatName(convertedBalance.response.from),
-      fiatSign: fiatSign(convertedBalance.response.from),
-    })
-  );
-
-  // console.log(coinData);
+  const coinData = await getCoinData();
+  const convertedBalanceData = await getConvertedBalanceData();
   // console.log(convertedBalanceData);
+  // console.log(coinData);
 
   return {
     props: {
