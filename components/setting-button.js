b9d672530d484413de1fbaf13500471d37b8fc14
Oliver Brownlow <olliebrownlow@gmail.com>
2022-03-17 22:55:24 +0000

    fixes bug where sign wasnt updating on new currency settings

--- components/setting-button.js
+++ components/setting-button.js
@@ -1,10 +1,10 @@
 const SettingButton = (props) => {
-
   return (
     <button
       className={props.className}
       name={props.name}
       value={props.value}
+      data-sign={props.sign}
       onClick={props.onClick}
       style={props.style}
     >
