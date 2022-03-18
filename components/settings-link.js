2fc8b6fbb412436af25c7ea2f28d9b98d822a5fe
Oliver Brownlow <olliebrownlow@gmail.com>
2022-03-18 06:02:43 +0000

    changes settings link to dynamically show flag and theme

--- components/settings-link.js
+++ components/settings-link.js
@@ -1,16 +1,20 @@
 import { useContext, useState, useEffect } from "react";
 import CurrencySettingsContext from "../context/currencySettings";
+import { getCookie } from "cookies-next";
+import { getCurrencyAndTheme } from "../actions";
 import Link from "next/link";
 import Image from "next/image";
 import eurFlag from "../public/eurFlagSmall.jpg";
 import gbpFlag from "../public/gbpFlagSmall.png";
 import usdFlag from "../public/usdFlagSmall.jpg";
 import styles from "./settingsLink.module.scss";
+import { Moon, Sun } from "react-feather";
 import { motion } from "framer-motion";
 
 const SettingsLink = () => {
   const { appCurrencyCode } = useContext(CurrencySettingsContext);
   const [currencyFlag, setCurrencyFlag] = useState(eurFlag);
+  const [currentTheme, setCurrentTheme] = useState("");
 
   useEffect(() => {
     if (appCurrencyCode === "gbp") {
@@ -20,19 +24,48 @@ const SettingsLink = () => {
     }
   }, [appCurrencyCode]);
 
+  useEffect(async () => {
+    const user = getCookie("ue");
+    const currencyAndTheme = await getCurrencyAndTheme(user);
+    setCurrentTheme(currencyAndTheme.theme);
+  }, []);
+
   return (
     <Link href="/settings">
       <motion.div
-        className={styles.currencyImg}
+        className={styles.split}
         whileTap={{ scale: 0.5 }}
         whileHover={{ scale: 1.1 }}
       >
-        <Image
-          src={currencyFlag}
-          alt={appCurrencyCode}
-          layout="intrinsic"
-          priority
-        />
+        {currentTheme === "dark" ? (
+          <>
+            <div
+              className={styles.left}
+              style={{
+                backgroundImage:
+                  "url(" + `/${appCurrencyCode}FlagSmall.jpg` + ")",
+                borderColor: "white",
+              }}
+            ></div>
+            <div className={styles.right}>
+              <Moon size={16} />
+            </div>
+          </>
+        ) : (
+          <>
+            <div
+              className={styles.left}
+              style={{
+                backgroundImage:
+                  "url(" + `/${appCurrencyCode}FlagSmall.jpg` + ")",
+                borderColor: "dimgrey",
+              }}
+            ></div>
+            <div className={styles.right}>
+              <Sun size={16} />
+            </div>
+          </>
+        )}
       </motion.div>
     </Link>
   );
