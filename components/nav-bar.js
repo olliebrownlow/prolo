4a2ccfd8ba2d7f01c4151866183ce0a95b030544
Oliver Brownlow <olliebrownlow@gmail.com>
2022-04-09 10:52:07 +0000

    hides nav bar when not logged in

--- components/nav-bar.js
+++ components/nav-bar.js
@@ -1,18 +1,28 @@
+import { useContext } from "react";
+import { UserContext } from "../lib/UserContext";
 import styles from "./navBar.module.scss";
 import navButtons from "../config/navButtons";
 import NavButton from "./nav-button";
 
-const NavBar = () => (
-  <div className={styles.NavBar}>
-    {navButtons.map((button) => (
-      <NavButton
-        key={button.path}
-        path={button.path}
-        label={button.label}
-        icon={button.icon}
-      />
-    ))}
-  </div>
-);
+const NavBar = () => {
+  const [user, setUser] = useContext(UserContext);
+
+  return (
+    <>
+      {user?.issuer && !user?.loading ? (
+        <div className={styles.NavBar}>
+          {navButtons.map((button) => (
+            <NavButton
+              key={button.path}
+              path={button.path}
+              label={button.label}
+              icon={button.icon}
+            />
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
+};
 
 export default NavBar;
